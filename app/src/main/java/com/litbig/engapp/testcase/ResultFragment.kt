cd9e4181feeb4ed10ed1171ebe2d662d1b25df7f
mslee0037 <lmskna12345@gmailcom>
2021-05-21 09:29:00 +0900

    layout update

--- app/src/main/java/com/litbig/engapp/testcase/ResultFragment.kt
+++ app/src/main/java/com/litbig/engapp/testcase/ResultFragment.kt
@@ -1,4 +1,4 @@
-package com.litbig.engapp
+package com.litbig.engapp.testcase
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.litbig.engapp.R
 import com.litbig.engapp.databinding.FragmentMainBinding
+import com.litbig.engapp.databinding.FragmentResultBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -15,15 +17,15 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [MainFragment.newInstance] factory method to
+ * Use the [ResultFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MainFragment : Fragment() {
+class ResultFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
-    lateinit var binding: FragmentMainBinding
+    lateinit var binding: FragmentResultBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -36,20 +38,13 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        binding = FragmentMainBinding.inflate(inflater, container, false)
+        binding = FragmentResultBinding.inflate(inflater, container, false)
         binding.myInstance = this
         return binding.root
     }
 
     fun onClick(view: View) {
-        when(view){
-            binding.btnGPS -> findNavController().navigate(R.id.action_mainFragment_to_gpsFragment)
-        }
-    }
-
-    fun endTest() {
-
+        findNavController().navigate(R.id.action_global_mainFragment)
     }
 
     companion object {
@@ -59,12 +54,12 @@ class MainFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment MainFragment.
+         * @return A new instance of fragment ResultFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            MainFragment().apply {
+            ResultFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
